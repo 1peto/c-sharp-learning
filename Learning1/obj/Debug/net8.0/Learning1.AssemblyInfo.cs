a64fcab83c2e8ee21f76260bd119becac23553c7
1peto <patrikragan9gmail.com>
2025-08-19 17:02:41 +0200

    oki

--- Learning1/obj/Debug/net8.0/Learning1.AssemblyInfo.cs
+++ Learning1/obj/Debug/net8.0/Learning1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb6bad711b0deb8732bf9df6941068c83fa6be3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0cc6999afa904d866b680aafb55af49f0b6e613")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
