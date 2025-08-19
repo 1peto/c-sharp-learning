1daf3c9b7ad3d4fc0164ecab87ef385b2935ee04
1peto <patrikragan9gmail.com>
2025-08-19 17:35:07 +0200

    Vyčistenie ConsoleApp1 - odstránené Visual Studio súbory (.vs/, .sln)

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a64fcab83c2e8ee21f76260bd119becac23553c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb81f95f68df169f2259771051dcc416bcbe657")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
