1a74e329c7e5d33d2a23054bca70527e618cc060
1peto <patrikragan9gmail.com>
2025-08-19 20:16:52 +0200

    priprava pre aktivaciu API

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -7,14 +7,25 @@ namespace WeatherApp
         static void Main(string[] args)
         {
             Console.WriteLine("=== WEATHER APP ===");
-            Console.WriteLine("Tu budeš programovať svoju weather aplikáciu!");
+            //fake simluacia API
+            Console.Write("Zadaj nazov mesta: ");
+            string mesto = Console.ReadLine();
+
+            Console.WriteLine($"Hladam pocasie pre {mesto}...");
+
+            Random random = new Random();
+            int teplota = random.Next(-5, 30);
+            string[] pocasie = { "slnecno", "oblačno", "dážď", "sneženie" };
+            string aktualnePocasie = pocasie[random.Next(pocasie.Length)];
+
+            //vypisy random ass udajov
+            Console.WriteLine($"Teplota: {teplota} °C");
+            Console.WriteLine($"Pocasie: {aktualnePocasie}");
+
+            Console.WriteLine("TODO - keď bude API aktívne:");
+            Console.WriteLine("- Nahradiť simuláciu skutočným HTTP requestom");
+            Console.WriteLine("- Spracovať JSON odpoveď z OpenWeatherMap");
             Console.WriteLine();
-            Console.WriteLine("TODO:");
-            Console.WriteLine("- Vytvoriť triedu pre prácu s weather API");
-            Console.WriteLine("- Implementovať získavanie údajov o počasí");
-            Console.WriteLine("- Pridať user interface");
-            Console.WriteLine();
-            Console.WriteLine("Začni programovať! ");
         }
     }
 }
